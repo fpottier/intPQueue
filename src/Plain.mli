d1f47103e8dc036abc8f603761373e5538b26a50
François Pottier <francois.pottier@inria.fr>
2025-09-24 11:39:44 +0200

    Clean up Plain.mli.

--- src/Plain.mli
+++ src/Plain.mli
@@ -16,30 +16,52 @@
 type priority =
   int
 
-(**The type of priority queues. *)
+(**A priority queue is an abstract mutable data structure. It can be thought
+   of as a bag of elements, where each element carries a certain priority. *)
 type 'a t
 
-(**[create()] creates an empty priority queue. *)
+(**[create()] creates an empty priority queue.
+
+   Time complexity: {m O(1)}. *)
 val create: unit -> 'a t
 
-(**[add q x p] inserts the element [x] with priority [p] into the queue [q]. *)
+(**[add q x p] inserts the element [x] with priority [p] into the queue [q].
+
+   Time complexity: {m O(1)} (amortized). *)
 val add: 'a t -> 'a -> priority -> unit
 
-(**[extract q] extracts an element with minimum priority out of the queue [q]
-   and returns it. *)
+(**[extract q] extracts an element out of the queue [q] and returns it. This
+   element has minimum priority among all of the elements that are currently
+   present in the queue. If the queue is empty, [None] is returned.
+
+  Time complexity: {m O(p)}. If the queue is used in a {i monotonic} manner
+  (that is, if the priority that is used in every call to {!add} is at least
+  as high as the priority of the last element that was returned by
+  {!extract}) then the time complexity of {m n} calls to {!extract} is only
+  {m O(n+p)}. Indeed, in this monotonic scenario, the cost of scanning the
+  queue's main array, so as to find the next element with minimum priority,
+  is shared between all invocations of {!extract}. *)
 val extract: 'a t -> 'a option
 
-(**[is_empty q] tests whether the queue [q] is empty. *)
+(**[is_empty q] tests whether the queue [q] is empty.
+
+   Time complexity: {m O(1)}. *)
 val is_empty: 'a t -> bool
 
-(**[cardinal q] returns the number of elements in the queue [q]. *)
+(**[cardinal q] returns the number of elements in the queue [q].
+
+   Time complexity: {m O(1)}. *)
 val cardinal: 'a t -> int
 
 (**[repeat q f] repeatedly extracts an element with minimum priority out of [q]
    and passes it to [f] (which may insert new elements into [q]), until [q] is
-   exhausted. *)
+   exhausted.
+
+   Time complexity: the total cost of {m n} calls to {!extract} and {m n}
+   invocations of the function [f]. *)
 val repeat: 'a t -> ('a -> unit) -> unit
 
 (**/**)
+
 (**[check] is used only during testing. *)
 val check : 'a t -> unit
